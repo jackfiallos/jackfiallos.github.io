be1fd103b825e52ebb21db8766793f6c35fcb08c
jackfiallos <erling.fiallos@qbit.com.mx>
2015-10-25 16:19:46 -0600

    web push notifications test

---
+++ README.md
@@ -0,0 +1,1 @@
+Jackfiallos github
---
+++ web-push-notifications/js/app.js
@@ -0,0 +1,52 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('https://jackfiallos.github.io/js/service-worker.js').then(
+        function(){
+            // service worker ?
+            if (!('showNotification' in ServiceWorkerRegistration.prototype)) {
+                console.warn('Las notificaciones no son soportadas');
+            return;
+        }
+
+        // Permissions check
+        if (Notification.permission === 'denied') {
+            console.warn('El usuario ha bloqueado las notificaciones');
+            return;
+        }
+
+        // Push messaging supported
+        if (!('PushManager' in window)) {
+            console.warn('La mensajería push no es soportada');
+            return;
+        }
+
+        navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+            serviceWorkerRegistration.pushManager.getSubscription().then(
+                function(subscription) {
+                    if (!subscription) {
+                        console.log(subscription);
+                    return;
+                    }
+                    else {
+                        console.log(subscription);
+                    }
+                }
+            ).catch(function(err) {
+                console.warn('Error durante getSubscription()', err);
+            });
+
+            serviceWorkerRegistration.pushManager.subscribe().then(
+                function(pushSubscription) {
+                    console.log(pushSubscription);
+                }, function(error) {
+                    console.log(error);
+                }
+            ).catch(function(err) {
+                console.warn('Error durante subscribe()', err);
+            });
+        });
+    }).catch(function(err) {
+        console.warn('Error durante subscribe()', err);
+    });
+} else {
+    console.warn('Los Service workers no están soportados en este navegador.');
+}
