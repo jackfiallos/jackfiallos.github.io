9a18435daafbef6bd7cd3a64a95af34c56b37891
jackfiallos <erling.fiallos@qbit.com.mx>
2015-10-25 19:37:26 -0600

    gcm it's done

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
 self.addEventListener('push', function(event) {
     console.log('Received a push message', event);
 
-    var title = 'GCM Jackfiallos';
-    var body = 'Contenido breve de la notificación push';
+    var title = 'Prueba de mensajería';
+    var body = 'Este podría ser el mensaje que se envía a los clientes suscritos :)';
     var icon = '/google-logo.jpg';
     var tag = 'push tag test jack';
 
